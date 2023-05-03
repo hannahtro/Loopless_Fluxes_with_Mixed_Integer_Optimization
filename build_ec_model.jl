1f74d5bec0d6bf02a4c0f5f2e2256dd39a9771a3
Troppens <htroppen@mob-009564.zib.de>
2023-05-03 17:37:08 +0200

    add model data

---
+++ build_ec_model.jl
@@ -0,0 +1,127 @@
+using COBREXA, COBREXA.Everything, JSON, CSV, Serialization
+
+include("build_utilities.jl")
+
+# Load base model
+model = load_model(ObjectModel, "e_coli_core.json")
+
+# add some transport costs
+for rid in [
+    "ACALDt", "ACt2r", "PYRt2", "SUCCt3", "ETOHt2r"
+]
+    model.reactions[rid].gene_associations = [Isozyme(["b2492"]), Isozyme(["b0904"])]
+end
+
+# remove spontaneous reaction genes
+rm_spontaneous!(model; spont_rxn_genes = ["s0001"])
+
+# classify reactions
+metabolic_reactions = String[]
+transport_reactions = String[]
+other_reactions = String[]
+pseudo_reactions = String[]
+for rid in reactions(model)
+    if is_metabolic_reaction(model, rid) && !is_membrane_reaction(model, rid)
+        push!(metabolic_reactions, rid)
+    elseif is_transport_reaction(model, rid)
+        push!(transport_reactions, rid)
+    elseif any(
+        f(model, rid) for f in [
+            is_atp_maintenance_reaction,
+            is_biomass_reaction,
+            is_boundary,
+            is_exchange_reaction,
+            is_pseudo_reaction,
+            is_spontaneous_reaction,
+        ]
+    )
+        push!(pseudo_reactions, rid) #  these get no kcat
+    else
+        push!(other_reactions, rid)
+    end
+end
+
+# load kcat data
+rid_kcat = Dict(
+    String(row.react_id) => row.kappmax_KO_ALE_davidi_per_pp_per_s_ensemble_model for
+    row in CSV.File("Dataset_S1C_turnonver_n.csv")
+)
+
+default_kcat = 65.0
+for rid in [metabolic_reactions; transport_reactions; other_reactions]
+    add_kcat!(model, rid, rid_kcat, default_kcat)
+end
+
+# load molar masses of gene products
+unassigned_genes = String[]
+gid_mm = JSON.parsefile("protein_masses.json")
+for gid in genes(model)
+    if haskey(gid_mm, gid)
+        model.genes[gid].product_molar_mass = gid_mm[gid]
+    else
+        push!(unassigned_genes, gid)
+    end
+end
+
+# load isozyme stoichiometry
+add_gene_association_stoichiometry!(model)
+
+# Fix constraints for exchange reactions
+for rid in ["EX_glc__D_e"] # unconstrain
+    model.reactions[rid].lower_bound = -1000.0
+    model.reactions[rid].upper_bound = 1000.0
+end
+
+# get genes of metabolic and transporter reactions
+transport_gids = String[]
+metabolic_gids = String[]
+for rid in reactions(model)
+    !has_reaction_grr(model, rid) && continue
+    if rid in metabolic_reactions
+        for grr in reaction_gene_associations(model, rid)
+            append!(metabolic_gids, grr)
+        end
+    elseif rid in [transport_reactions; other_reactions]
+        for grr in reaction_gene_associations(model, rid)
+            append!(transport_gids, grr)
+        end
+    end
+end
+unique!(transport_gids)
+unique!(metabolic_gids)
+
+setdiff(genes(model), [transport_gids; metabolic_gids])
+intersect(transport_gids, metabolic_gids)
+
+# fix some stuff
+isos = model.reactions["ATPS4r"].gene_associations
+for i in 1:length(isos)
+    all(values(isos[1].gene_product_stoichiometry) .== 1) && deleteat!(model.reactions["ATPS4r"].gene_associations, 1)
+end
+
+for rid in reactions(model)
+    isos = reaction_isozymes(model, rid)
+    isnothing(isos) && continue
+    for iso in isos 
+        for (k, v) in iso.gene_product_stoichiometry
+            if isnothing(model.genes[k].product_molar_mass)
+                model.genes[k].product_molar_mass = 40.0 # 
+            end
+        end
+    end 
+end
+
+# save model
+serialize("ec_e_coli_core.js", model)
+
+# save relevant IDs
+serialize("metabolic_gids.js", metabolic_gids)
+serialize("transport_gids.js", transport_gids)
+serialize("metabolic_rids.js", metabolic_reactions)
+serialize("transport_rids.js", [transport_reactions; other_reactions])
+
+for rid in reactions(model)
+    if isnothing(model.reactions[rid].gene_associations)
+        println(rid)
+    end
+end
---
+++ run_simple_enzyme_model.jl
@@ -0,0 +1,49 @@
+using Tulip, COBREXA, Serialization, COBREXA.Everything
+
+model = deserialize("ec_e_coli_core.js")
+
+m_rids = deserialize("metabolic_rids.js")
+t_rids = deserialize("transport_rids.js")
+
+m_gids = deserialize("metabolic_gids.js")
+t_gids = deserialize("transport_gids.js")
+
+# simplified enzyme constrained model (SMOMENT)
+smodel = make_simplified_enzyme_constrained_model(
+    model,
+    reaction_mass_groups = Dict(
+        "cytosol" => m_rids,
+        "membrane" => t_rids,
+    ),
+    reaction_mass_group_bounds = Dict(
+        "cytosol" => 75.0,
+        "membrane" => 75.0,
+    ),
+)
+
+sol = flux_balance_analysis(smodel, Tulip.Optimizer)
+
+fluxes = values_dict(:reaction, sol)
+fluxes["BIOMASS_Ecoli_core_w_GAM"]
+flux_summary(fluxes)
+
+# Full enzyme constrained model (GECKO formulation)
+ecmodel = make_enzyme_constrained_model(
+    model,
+    gene_product_mass_groups = Dict(
+        "cytosol" => m_gids,
+        "membrane" => t_gids,
+    ),
+    gene_product_mass_group_bounds = Dict(
+        "cytosol" => 75.0,
+        "membrane" => 75.0,
+    ),
+)
+
+sol = flux_balance_analysis(ecmodel, Tulip.Optimizer)
+
+fluxes = values_dict(:reaction, sol)
+fluxes["BIOMASS_Ecoli_core_w_GAM"]
+@show flux_summary(fluxes)
+
+enzs = values_dict(:enzyme, sol)
