68f86ac09d740c3a805206ac85b39f88ac3a949d
Troppens <htroppen@mob-009564.zib.de>
2023-05-09 14:17:14 +0200

    block reaction

--- COBREXA_JuMP.jl
+++ COBREXA_JuMP.jl
@@ -23,45 +23,61 @@ set_attribute(model, MOI.Silent(), true)
 # get reference to variables
 x = model[:x]
 
-# modify objective
-# set_objective_coefficient(model, x[1], 0.25)
-# @show objective_function(model)
-
 optimize!(model)
+reference_flux =  MOI.get.(model, MOI.VariablePrimal(), x)
+# @show reference_flux
+
 # @show MOI.get(model, MOI.TerminationStatus())
 @show MOI.get(model, MOI.ObjectiveValue())
 # @show  MOI.get.(model, MOI.VariablePrimal(), x)
 
 # loopless model
-internal_rxn_idxs = [
-    ridx for (ridx, rid) in enumerate(variables(molecular_model)) if
-    !is_boundary(reaction_stoichiometry(molecular_model, rid))
-]
+# internal_rxn_idxs = [
+#     ridx for (ridx, rid) in enumerate(variables(molecular_model)) if
+#     !is_boundary(reaction_stoichiometry(molecular_model, rid))
+# ]
 
-@show length(internal_rxn_idxs)
+# @show length(internal_rxn_idxs)
 
-N_int = nullspace(Array(stoichiometry(molecular_model)[:, internal_rxn_idxs])) # no sparse nullspace function
+# N_int = nullspace(Array(stoichiometry(molecular_model)[:, internal_rxn_idxs])) # no sparse nullspace function
 
-a = @variable(model, a[1:length(internal_rxn_idxs)], Bin)
-G = @variable(model, G[1:length(internal_rxn_idxs)]) # approx ΔG for internal reactions
+# a = @variable(model, a[1:length(internal_rxn_idxs)], Bin)
+# G = @variable(model, G[1:length(internal_rxn_idxs)]) # approx ΔG for internal reactions
 
-for (cidx, ridx) in enumerate(internal_rxn_idxs)
-    @constraint(model, -1000 * (1 - a[cidx]) <= x[ridx])
-    @constraint(model, x[ridx] <= 1000 * a[cidx])
+# for (cidx, ridx) in enumerate(internal_rxn_idxs)
+#     @constraint(model, -1000 * (1 - a[cidx]) <= x[ridx])
+#     @constraint(model, x[ridx] <= 1000 * a[cidx])
 
-    @constraint(
-        model,
-        -1000 * a[cidx] + (1 - a[cidx]) <= G[cidx]
-    )
-    @constraint(
-        model,
-        G[cidx] <=
-        -a[cidx] + 1000 * (1 - a[cidx])
-    )
-end
+#     @constraint(
+#         model,
+#         -1000 * a[cidx] + (1 - a[cidx]) <= G[cidx]
+#     )
+#     @constraint(
+#         model,
+#         G[cidx] <=
+#         -a[cidx] + 1000 * (1 - a[cidx])
+#     )
+# end
 
-@constraint(model, N_int' * G .== 0)
+# @constraint(model, N_int' * G .== 0)
+
+# @show model
+# optimize!(model)
+# @show MOI.get(model, MOI.ObjectiveValue())
+
+
+# block reaction
+set_lower_bound(x[1], 0)
+set_upper_bound(x[1], 0)
 
 @show model
+
 optimize!(model)
+knock_out_flux =  MOI.get.(model, MOI.VariablePrimal(), x)
+# @show knock_out_flux
 @show MOI.get(model, MOI.ObjectiveValue())
+
+# MOMA
+# modify objective
+# set_objective_coefficient(model, x[1], 0.25)
+# @show objective_function(model)
---
+++ create_model.jl
@@ -0,0 +1,23 @@
+using COBREXA, COBREXA.Everything, Serialization
+
+model = ObjectModel()
+
+metabolite_list = [Metabolite(string("m_", num)) for num = 1:4]
+add_metabolites!(model, metabolite_list)
+
+
+r_1 = ReactionBidirectional("r_1", Dict("m_1" => 1.0))
+r_2 = ReactionForward("r_2", Dict("m_1" => -2.0, "m_2" => 1.0, "m_3" => 1.0))
+r_3 = ReactionBidirectional("r_3", Dict("m_2" => -1.0))
+r_4 = ReactionBidirectional("r_4", Dict("m_3" => -1.0))
+r_5 = ReactionForward("r_5", Dict("m_4" => -1.0, "m_1" => 1.0, "m_3" => 3.0))
+r_6 = ReactionForward("r_6", Dict("m_4" => 1.0))
+
+add_reactions!(model, [r_1, r_2, r_3, r_4, r_5, r_6])
+
+@show metabolites(model)
+@show reactions(model)
+
+# save model
+serialize("test_model.js", model)
+save_model(model, "test_model.json")
