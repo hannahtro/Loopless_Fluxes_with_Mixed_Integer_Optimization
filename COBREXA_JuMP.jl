09fb39e237d6325b3182023bb9448d62a3869c60
Troppens <htroppen@mob-009564.zib.de>
2023-05-09 10:32:46 +0200

    loopless FBA

--- COBREXA_JuMP.jl
+++ COBREXA_JuMP.jl
@@ -1,6 +1,7 @@
 
 using COBREXA, Serialization, COBREXA.Everything
 using SCIP, JuMP
+using LinearAlgebra
 
 optimizer = SCIP.Optimizer
 
@@ -15,7 +16,52 @@ println("objective : ", molecular_model.objective)
 println("")
 
 model = make_optimization_model(molecular_model, optimizer)
+set_attribute(model, MOI.Silent(), true)
 @show model
 @show objective_function(model)
 
-set_objective_coefficient(model, model.x[1], 0.25)
+# get reference to variables
+x = model[:x]
+
+# modify objective
+# set_objective_coefficient(model, x[1], 0.25)
+# @show objective_function(model)
+
+optimize!(model)
+# @show MOI.get(model, MOI.TerminationStatus())
+@show MOI.get(model, MOI.ObjectiveValue())
+# @show  MOI.get.(model, MOI.VariablePrimal(), x)
+
+# loopless model
+internal_rxn_idxs = [
+    ridx for (ridx, rid) in enumerate(variables(molecular_model)) if
+    !is_boundary(reaction_stoichiometry(molecular_model, rid))
+]
+
+@show length(internal_rxn_idxs)
+
+N_int = nullspace(Array(stoichiometry(molecular_model)[:, internal_rxn_idxs])) # no sparse nullspace function
+
+a = @variable(model, a[1:length(internal_rxn_idxs)], Bin)
+G = @variable(model, G[1:length(internal_rxn_idxs)]) # approx ΔG for internal reactions
+
+for (cidx, ridx) in enumerate(internal_rxn_idxs)
+    @constraint(model, -1000 * (1 - a[cidx]) <= x[ridx])
+    @constraint(model, x[ridx] <= 1000 * a[cidx])
+
+    @constraint(
+        model,
+        -1000 * a[cidx] + (1 - a[cidx]) <= G[cidx]
+    )
+    @constraint(
+        model,
+        G[cidx] <=
+        -a[cidx] + 1000 * (1 - a[cidx])
+    )
+end
+
+@constraint(model, N_int' * G .== 0)
+
+@show model
+optimize!(model)
+@show MOI.get(model, MOI.ObjectiveValue())
