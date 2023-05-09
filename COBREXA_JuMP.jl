3ad8a9c39e0a373f75a57f059e25c5bbe8db02b1
Troppens <htroppen@mob-009564.zib.de>
2023-05-09 16:24:11 +0200

    small changes

--- COBREXA_JuMP.jl
+++ COBREXA_JuMP.jl
@@ -13,8 +13,10 @@ println("number of genes : ", length(molecular_model.genes))
 # @show model.annotations
 # @show model.notes
 println("objective : ", molecular_model.objective)
-println("")
 
+println("")
+println("FBA")
+println("----------------------------------")
 model = make_optimization_model(molecular_model, optimizer)
 set_attribute(model, MOI.Silent(), true)
 @show model
@@ -31,41 +33,46 @@ reference_flux =  MOI.get.(model, MOI.VariablePrimal(), x)
 @show MOI.get(model, MOI.ObjectiveValue())
 # @show  MOI.get.(model, MOI.VariablePrimal(), x)
 
+println("")
+println("loopless FBA")
+println("----------------------------------")
 # loopless model
-# internal_rxn_idxs = [
-#     ridx for (ridx, rid) in enumerate(variables(molecular_model)) if
-#     !is_boundary(reaction_stoichiometry(molecular_model, rid))
-# ]
+internal_rxn_idxs = [
+    ridx for (ridx, rid) in enumerate(variables(molecular_model)) if
+    !is_boundary(reaction_stoichiometry(molecular_model, rid))
+]
 
-# @show length(internal_rxn_idxs)
+@show length(internal_rxn_idxs)
 
-# N_int = nullspace(Array(stoichiometry(molecular_model)[:, internal_rxn_idxs])) # no sparse nullspace function
+N_int = nullspace(Array(stoichiometry(molecular_model)[:, internal_rxn_idxs])) # no sparse nullspace function
 
-# a = @variable(model, a[1:length(internal_rxn_idxs)], Bin)
-# G = @variable(model, G[1:length(internal_rxn_idxs)]) # approx ΔG for internal reactions
+a = @variable(model, a[1:length(internal_rxn_idxs)], Bin)
+G = @variable(model, G[1:length(internal_rxn_idxs)]) # approx ΔG for internal reactions
 
-# for (cidx, ridx) in enumerate(internal_rxn_idxs)
-#     @constraint(model, -1000 * (1 - a[cidx]) <= x[ridx])
-#     @constraint(model, x[ridx] <= 1000 * a[cidx])
+for (cidx, ridx) in enumerate(internal_rxn_idxs)
+    @constraint(model, -1000 * (1 - a[cidx]) <= x[ridx])
+    @constraint(model, x[ridx] <= 1000 * a[cidx])
 
-#     @constraint(
-#         model,
-#         -1000 * a[cidx] + (1 - a[cidx]) <= G[cidx]
-#     )
-#     @constraint(
-#         model,
-#         G[cidx] <=
-#         -a[cidx] + 1000 * (1 - a[cidx])
-#     )
-# end
+    @constraint(
+        model,
+        -1000 * a[cidx] + (1 - a[cidx]) <= G[cidx]
+    )
+    @constraint(
+        model,
+        G[cidx] <=
+        -a[cidx] + 1000 * (1 - a[cidx])
+    )
+end
 
-# @constraint(model, N_int' * G .== 0)
+@constraint(model, N_int' * G .== 0)
 
-# @show model
-# optimize!(model)
-# @show MOI.get(model, MOI.ObjectiveValue())
+@show model
+optimize!(model)
+@show MOI.get(model, MOI.ObjectiveValue())
 
-
+println("")
+println("loopless FBA with blocked reaction")
+println("----------------------------------")
 # block reaction
 set_lower_bound(x[1], 0)
 set_upper_bound(x[1], 0)
@@ -77,6 +84,9 @@ knock_out_flux =  MOI.get.(model, MOI.VariablePrimal(), x)
 # @show knock_out_flux
 @show MOI.get(model, MOI.ObjectiveValue())
 
+println("")
+println("loopless MOMA")
+println("----------------------------------")
 # MOMA
 # modify objective
 L = - reference_flux
