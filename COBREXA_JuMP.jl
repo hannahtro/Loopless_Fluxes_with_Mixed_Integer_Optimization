c9495365f7ee09d70ee0b3c74e46ba5cdb2d2c43
Troppens <htroppen@mob-009564.zib.de>
2023-05-10 12:40:35 +0200

    MOMA solved with boscia

--- COBREXA_JuMP.jl
+++ COBREXA_JuMP.jl
@@ -2,6 +2,7 @@
 using COBREXA, Serialization, COBREXA.Everything
 using SCIP, JuMP
 using LinearAlgebra
+using Boscia, FrankWolfe
 
 optimizer = SCIP.Optimizer
 
@@ -93,5 +94,27 @@ L = - reference_flux
 Q = I(length(x))
 @objective(model, Min, 1/2 * x' * Q * x + L' * x)
 # @show objective_function(model)
+@show typeof(objective_function(model))
 optimize!(model)
 @show MOI.get(model, MOI.ObjectiveValue())
+
+println("")
+println("loopless MOMA in Boscia")
+println("----------------------------------")
+
+function f(x)
+    length_var = size(Q)[1]
+    f_x = 1/2 * x[1:length_var]' * Q * x[1:length_var] + L' * x[1:length_var]
+end
+# @show f(ones(length(x)))
+
+function grad!(storage, x)
+    length_var = size(Q)[1]
+    storage[1:length_var] = Q * x[1:length_var] + L
+    storage[length_var+1:length(x)] .= 0
+end
+
+set_objective_sense(model, FEASIBILITY_SENSE)
+moi_model = backend(model)
+lmo = FrankWolfe.MathOptLMO(moi_model)
+x, _, result = Boscia.solve(f, grad!, lmo, verbose=true)
