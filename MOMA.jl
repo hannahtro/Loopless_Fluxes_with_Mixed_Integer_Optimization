5ba6db05ef30b6c8a24e06d09fa1b0aa77042e87
Troppens <htroppen@mob-009564.zib.de>
2023-05-16 16:46:28 +0200

    add model data

--- MOMA.jl
+++ MOMA.jl
@@ -2,119 +2,65 @@ using COBREXA, Serialization, COBREXA.Everything
 using SCIP, JuMP, Tulip
 using LinearAlgebra
 using Boscia, FrankWolfe
+using DataFrames, CSV
+
 include("functions.jl")
 
-# build model
-optimizer = SCIP.Optimizer
-# optimizer = Tulip.Optimizer
+function json_to_js(organism="iML1515")
+    # could not extract json.gz
+    molecular_model = load_model(ObjectModel, "data/" * organism * ".json")
+    serialize("data/" * organism * ".js", molecular_model)
+end
 
-molecular_model = deserialize("data/ec_e_coli_core.js")
+function get_fba_data(organism="iML1515",)
+    # build model
+    optimizer = SCIP.Optimizer
 
-print_model(molecular_model, "E COLI COBRA MODEL")
+    molecular_model = deserialize("data/" * organism * ".js")
+    print_model(molecular_model)
 
-# add loop
-r_1 = ReactionBidirectional("r_1", Dict("glc__D_e" => -1.0, "gln__L_c" => 1.0))
-r_2 = ReactionBidirectional("r_2", Dict("gln__L_c" => -1.0, "gln__L_e" => 1.0))
-r_3 = ReactionBidirectional("r_3", Dict("gln__L_e" => -1.0, "glc__D_e" => 1.0))
+    model = make_optimization_model(molecular_model, optimizer)
+    @show model
+    set_attribute(model, MOI.Silent(), true)
 
-add_reactions!(molecular_model, [r_1, r_2, r_3])
+    # FBA
+    type = "fba"
+    objective_fba, vars_fba, time_fba, termination_fba = optimize_model(model, print_objective=true)
 
-model = make_optimization_model(molecular_model, optimizer)
-@show model
-set_attribute(model, MOI.Silent(), true)
+    df = DataFrame(
+        objective_fba=objective_fba, 
+        vars_fba=vars_fba, 
+        time_fba=time_fba, 
+        termination_fba=termination_fba)
 
-# # FBA
-# optimize_model(model, print_objective=true)
+    file_name = joinpath(@__DIR__,"csv/" * organism * "_" * type * ".csv")
 
-# FBA with modified objective
-x = model[:x]
-@objective(model, Max, sum(x))
-optimize_model(model)
+    if !isfile(file_name)
+        CSV.write(file_name, df, append=true, writeheader=true)
+    else 
+        CSV.write(file_name, df, append=true)    
+    end
 
-# loopless FBA
-add_loopless_constraints(molecular_model, model)
-optimize_model(model, "loopless FBA")
+    # loopless FBA
+    type = "loopless_fba"
+    time_limit = 1800
+    add_loopless_constraints(molecular_model, model)
+    @show model
+    set_attribute(model, MOI.Silent(), false)
+    objective_loopless_fba, vars_loopless_fba, time_loopless_fba, termination_loopless_fba = 
+        optimize_model(model, "loopless FBA", time_limit=time_limit)
 
-# loopless FBA max x[25]
-x = model[:x]
-@objective(model, Max, x[25])
-optimize_model(model, "loopless FBA without blocked reaction", print_objective=true)
-reference_flux =  MOI.get.(model, MOI.VariablePrimal(), x)
+    df = DataFrame(
+        objective_loopless_fba=objective_loopless_fba, 
+        vars_loopless_fba=vars_loopless_fba, 
+        time_loopless_fba=time_loopless_fba, 
+        termination_loopless_fba=termination_loopless_fba)
 
-# FBA on blocked reaction
-# block reaction
-set_lower_bound(x[1], 0)
-set_upper_bound(x[1], 0)
-optimize_model(model, "loopless FBA with blocked reaction")
-# knock_out_flux =  MOI.get.(model, MOI.VariablePrimal(), x)
+    file_name = joinpath(@__DIR__,"csv/" * organism * "_" * type * "_" * string(time_limit) * ".csv")
 
-# MOMA
-# @objective(model, Min, 1/2 * x' * Q * x + L' * x)
-# # @show objective_function(model)
-# # @show typeof(objective_function(model))
-# optimize_model(model, "loopless MOMA")
-# moma(model, x, reference_flux)
-# unresolved numerical troubles in LP 1022 cannot be dealt with
-
-# MOMA with Boscia 
-# moma_boscia(model, x, reference_flux)
-# objective value : -5.07598806e6
-
-# FBA with enzyme constraints
-# basic_model = deserialize("data/ec_e_coli_core.js")
-
-# m_rids = deserialize("data/metabolic_rids.js")
-# t_rids = deserialize("data/transport_rids.js")
-# m_gids = deserialize("data/metabolic_gids.js")
-# t_gids = deserialize("data/transport_gids.js")
-
-# smodel = make_simplified_enzyme_constrained_model(
-#     basic_model,
-#     reaction_mass_groups = Dict(
-#         "cytosol" => m_rids,
-#         "membrane" => t_rids,
-#     ),
-#     # total mass limit of each group of reactions
-#     reaction_mass_group_bounds = Dict(
-#         "cytosol" => 75.0,
-#         "membrane" => 75.0,
-#     ),
-# )
-
-# optimizer = SCIP.Optimizer
-# model = make_optimization_model(smodel, optimizer)
-# set_attribute(model, MOI.Silent(), true)
-# optimize_model(model, "enzyme-constrained FBA")
-
-# # ecFBA with loops 
-# r_1 = ReactionBidirectional("r_1", Dict("glc__D_e" => -1.0, "gln__L_c" => 1.0))
-# r_2 = ReactionBidirectional("r_2", Dict("gln__L_c" => -1.0, "gln__L_e" => 1.0))
-# r_3 = ReactionBidirectional("r_3", Dict("gln__L_e" => -1.0, "glc__D_e" => 1.0))
-
-# add_reactions!(basic_model, [r_1, r_2, r_3])
-# smodel = make_simplified_enzyme_constrained_model(
-#     basic_model,
-#     reaction_mass_groups = Dict(
-#         "cytosol" => m_rids,
-#         "membrane" => t_rids,
-#     ),
-#     # total mass limit of each group of reactions
-#     reaction_mass_group_bounds = Dict(
-#         "cytosol" => 75.0,
-#         "membrane" => 75.0,
-#     ),
-# )
-
-# optimizer = SCIP.Optimizer
-# model = make_optimization_model(smodel, optimizer)
-# set_attribute(model, MOI.Silent(), true)
-# optimize_model(model, "enzyme-constrained FBA with loops")
-
-# loopless ecFBA 
-# add_loopless_constraints(smodel, model)
-# optimize_model(model, "loopless ecFBA")
-# DOES NOT WORK YET
-
-# blocked loopless ecFBA
-
-# MOMA loopless ecFBA
+    if !isfile(file_name)
+        CSV.write(file_name, df, append=true, writeheader=true)
+    else 
+        CSV.write(file_name, df, append=true)    
+    end
+end
