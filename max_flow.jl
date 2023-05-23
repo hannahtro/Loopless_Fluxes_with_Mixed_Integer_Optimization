cddee42413e7b722a3b58c6f95e35e2ddb8569f8
Troppens <htroppen@mob-009564.zib.de>
2023-05-23 15:43:50 +0200

    get original reactions

--- max_flow.jl
+++ max_flow.jl
@@ -24,13 +24,13 @@ function max_flow(S_transform, lb_transform, ub_transform; optimizer=SCIP.Optimi
     # @show solution
 end
 
-function ubound_cycles(S_transform, solution)
+function ubounded_cycles(S_transform, solution)
     # delete non used reactions
     println("")
-    @show size(S_transform)
+    # @show size(S_transform)
     # @show S_transform
     non_zero_reactions = findall(!iszero,solution)
-    @show non_zero_reactions
+    # @show non_zero_reactions
     S_transform_reduced = []
     for (id,row) in enumerate(eachcol(S_transform))
         if id in non_zero_reactions
@@ -71,8 +71,28 @@ function ubound_cycles(S_transform, solution)
     # compute cycles
     cycles = simplecycles(G)
     # @show length(cycles)
-    return cycles
+    return cycles, edge_mapping
 end 
+
+function unbounded_cycles_S(cycles, edge_mapping)
+    edge_mapping_reverse = Dict(value => key for (key, value) in edge_mapping)
+    # @show edge_mapping_reverse
+
+    unbounded_cycles = []
+    for cycle in cycles
+        unbounded_cycle = []
+        temp_cycle = deepcopy(cycle)
+        append!(temp_cycle, cycle[1])
+        # @show temp_cycle
+        # @show cycle
+        for (idx,m) in enumerate(cycle)
+            key = [m,temp_cycle[idx+1]]
+            push!(unbounded_cycle,(edge_mapping_reverse[key]))
+        end
+        push!(unbounded_cycles, unbounded_cycle)
+    end
+    return unbounded_cycles
+end
 
 # test network
 S = [[0,1,1,-1,0] [-1,1,1,0,0] [0,0,-1,0,1] [0,0,0,1,-1]]
@@ -89,10 +109,12 @@ solution = max_flow(S_transform, lb_transform, ub_transform; optimizer=SCIP.Opti
 @assert length(solution) == size(S_transform)[2]
 
 # get original reactions
-cycles = ubound_cycles(S_transform, solution)
+cycles, edge_mapping = ubounded_cycles(S_transform, solution)
 @show cycles
+@show edge_mapping
 
-# @show edge_mapping
+unbounded_cycles = unbounded_cycles_S(cycles, edge_mapping)
+@show unbounded_cycles
 
 # # test organism
 # organism = "iAF692"
