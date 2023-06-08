f278e5c7172b144a2795b085490ad4ad3ee9a7db
hannahtro <jaimelabaguette@gmail.com>
2023-06-08 16:13:19 +0200

    models do not match after adding loopless constraints

--- csv/compare_models.jl
+++ csv/compare_models.jl
@@ -4,11 +4,29 @@
 #     println("different")
 # end
 
-if success(`cmp --quiet model_vector_iAF692_laptop.lp model_vector_iAF692_server.lp`)
+if success(`cmp --quiet model_cobrexa_iAF692_laptop.lp model_cobrexa_iAF692.lp`)
     println("same")
 else
     println("different")
 end
 
-diff_output = read(ignorestatus(`diff model_vector_iAF692_laptop.lp model_vector_iAF692_server.lp`), String)
+if success(`cmp --quiet model_cobrexa_iJR904_laptop.lp model_cobrexa_iJR904.lp`)
+    println("same")
+else
+    println("different")
+end
+
+if success(`cmp --quiet model_vector_iAF692_laptop.lp model_vector_iAF692.lp`)
+    println("same")
+else
+    println("different")
+end
+
+if success(`cmp --quiet model_vector_iJR904_laptop.lp model_vector_iJR904.lp`)
+    println("same")
+else
+    println("different")
+end
+
+diff_output = read(ignorestatus(`diff model_vector_iAF692_laptop.lp model_vector_iAF692.lp`), String)
 @show diff_output
