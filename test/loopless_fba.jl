bd5c04993972fd79c80358abac9910972ec171ee
hannahtro <jaimelabaguette@gmail.com>
2023-06-12 11:01:24 +0200

    set up run

--- test/loopless_fba.jl
+++ test/loopless_fba.jl
@@ -2,24 +2,24 @@ include("../src/loopless_fba.jl")
 
 @show VERSION
 
-organism = "iAF692"
-loopless_fba_blocked_data(organism, time_limit=1800, ceiling=10000, same_objective=false, nullspace_formulation=false)
-loopless_fba_blocked_data(organism, time_limit=1800, ceiling=10000, same_objective=false, shortest_cycles=true, nullspace_formulation=false)
-loopless_fba_blocked_data(organism, time_limit=1800, ceiling=10000, same_objective=false, shortest_cycles=true, block_limit=50, nullspace_formulation=false)
-loopless_fba_blocked_data(organism, time_limit=1800, ceiling=10000, same_objective=false, block_limit=50, nullspace_formulation=false)
+# organism = "iAF692"
+# loopless_fba_blocked_data(organism, time_limit=1800, ceiling=10000, same_objective=false, nullspace_formulation=false)
+# loopless_fba_blocked_data(organism, time_limit=1800, ceiling=10000, same_objective=false, shortest_cycles=true, nullspace_formulation=false)
+# loopless_fba_blocked_data(organism, time_limit=1800, ceiling=10000, same_objective=false, shortest_cycles=true, block_limit=50, nullspace_formulation=false)
+# loopless_fba_blocked_data(organism, time_limit=1800, ceiling=10000, same_objective=false, block_limit=50, nullspace_formulation=false)
 
 # loopless_fba_data(organism, time_limit=1800, nullspace_formulation=true, nullspace_formulation=false)
 # loopless_fba_data(organism, time_limit=1800, nullspace_formulation=false, nullspace_formulation=false)
 
 
 organism = "iJR904"
-loopless_fba_blocked_data(organism, time_limit=1800, ceiling=10000, same_objective=false, nullspace_formulation=false)
-loopless_fba_blocked_data(organism, time_limit=1800, ceiling=10000, same_objective=false, shortest_cycles=true, nullspace_formulation=false)
-loopless_fba_blocked_data(organism, time_limit=1800, ceiling=10000, same_objective=false, shortest_cycles=true, block_limit=50, nullspace_formulation=false)
-loopless_fba_blocked_data(organism, time_limit=1800, ceiling=10000, same_objective=false, block_limit=50, nullspace_formulation=false)
+# loopless_fba_blocked_data(organism, time_limit=1800, ceiling=10000, same_objective=false, nullspace_formulation=false)
+# loopless_fba_blocked_data(organism, time_limit=1800, ceiling=10000, same_objective=false, shortest_cycles=true, nullspace_formulation=false)
+# loopless_fba_blocked_data(organism, time_limit=1800, ceiling=10000, same_objective=false, shortest_cycles=true, block_limit=50, nullspace_formulation=false)
+# loopless_fba_blocked_data(organism, time_limit=1800, ceiling=10000, same_objective=false, block_limit=50, nullspace_formulation=false)
 
 
-loopless_fba_data(organism, time_limit=1800, nullspace_formulation=false, nullspace_formulation=false)
+loopless_fba_data(organism, time_limit=1800, nullspace_formulation=false)
 
 loopless_indicator_fba_data(organism, time_limit=1800, nullspace_formulation=false)
 loopless_indicator_fba_blocked_data(organism; time_limit=1800, ceiling=10, nullspace_formulation=false)
