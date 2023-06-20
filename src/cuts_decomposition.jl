c5db938b4ace9f164eab2d620e4ec4f909b99a8b
Troppens <htroppen@mob-009564.zib.de>
2023-06-20 11:00:28 +0200

    setup no good cut run

--- src/cuts_decomposition.jl
+++ src/cuts_decomposition.jl
@@ -1,8 +1,9 @@
+using Dates 
+
 include("optimization_model.jl")
 include("loopless_constraints.jl")
 
-
-function no_good_cuts(model, internal_rxn_idxs, S)
+function no_good_cuts(model, internal_rxn_idxs, S; time_limit=1800)
     x = model[:x]
     m, num_reactions = size(S)
 
@@ -14,15 +15,17 @@ function no_good_cuts(model, internal_rxn_idxs, S)
         @constraint(model, !a[cidx] => {x[ridx] + eps() <= 0})
     end
     # @objective(model, Max, 0)
+    # println("here")
 
-    _, _, solution, _, _ = optimize_model(model)
+    start_time = time()
+    # println("here")
+    objective_value, dual_bound, solution, _, termination = optimize_model(model)
     # @show solution
     solutions = [round.(solution, digits=5)]
     cuts = []
-
     # print(model)
     iter = 1
-    while !thermo_feasible_mu(internal_rxn_idxs,solution[internal_rxn_idxs], S)
+    while !thermo_feasible_mu(internal_rxn_idxs,solution[internal_rxn_idxs], S) && time()-start_time < time_limit
         @show iter
         # @show thermo_feasible_mu(internal_rxn_idxs,solution[internal_rxn_idxs], S)
         solution_a = solution[num_reactions+1:end]
@@ -44,7 +47,7 @@ function no_good_cuts(model, internal_rxn_idxs, S)
         push!(cuts,[cut])
         # print(model)
 
-        _, _, solution, _, _ = optimize_model(model)
+        objective_value, dual_bound, solution, _, termination = optimize_model(model)
         solution = round.(solution, digits=5)
         solution_a = solution[num_reactions+1:end]
 
@@ -57,5 +60,42 @@ function no_good_cuts(model, internal_rxn_idxs, S)
     end
     # @show cuts
     # @show solutions
-    return solution
+    end_time = time()
+    time_taken = end_time - start_time
+    @show time_taken
+
+    return objective_value, dual_bound, solution, time_taken, termination, iter
+end
+
+function no_good_cuts_data(organism; time_limit=1800, csv=true)
+    model = deserialize("../data/" * organism * ".js")
+    print_model(model, "organism")
+
+    S = stoichiometry(model)
+    lb, ub = bounds(model)
+    internal_rxn_idxs = [
+        ridx for (ridx, rid) in enumerate(variables(model)) if
+        !is_boundary(reaction_stoichiometry(model, rid))
+    ]
+
+    model = build_model(S, lb, ub)
+    objective_value, dual_bound, solution, time, termination, iter = no_good_cuts(model, internal_rxn_idxs, S, time_limit=time_limit)
+
+    thermo_feasible = thermo_feasible_mu(internal_rxn_idxs,solution[internal_rxn_idxs], S)
+
+    df = DataFrame(
+        objective_value=objective_value, 
+        dual_bound=dual_bound,
+        solution=solution, 
+        time=time, 
+        termination=termination,
+        time_limit=time_limit, 
+        thermo_feasible=thermo_feasible,
+        iter=iter)
+
+    type = "no_good_cuts"
+    file_name = joinpath(@__DIR__,"../csv/" * organism * "_" * type * "_" * string(time_limit) * ".csv")
+    if csv
+        CSV.write(file_name, df, append=false, writeheader=true)
+    end
 end
--- test/no_good_cuts.jl
+++ test/no_good_cuts.jl
@@ -1,4 +1,6 @@
 using Test 
+using DataFrames
+using CSV
 
 include("../src/cuts_decomposition.jl")
 
@@ -12,7 +14,7 @@ include("../src/cuts_decomposition.jl")
     model = build_model(S, lb, ub)
     internal_rxn_idxs = [2,3,4]
 
-    solution = no_good_cuts(model, internal_rxn_idxs, S)
+    objective_value, dual_bound, solution, time, termination, iter = no_good_cuts(model, internal_rxn_idxs, S)
 
     @test thermo_feasible_mu(internal_rxn_idxs,solution[internal_rxn_idxs], S)
 end
@@ -31,8 +33,16 @@ end
     ]
 
     model = build_model(S, lb, ub)
-    solution = no_good_cuts(model, internal_rxn_idxs, S)
 
-    @test thermo_feasible_mu(internal_rxn_idxs,solution[internal_rxn_idxs], S)
+    time_limit = 2
+    objective_value, dual_bound, solution, time, termination, iter = no_good_cuts(model, internal_rxn_idxs, S, time_limit=time_limit)
+
+    try 
+        thermo_feasible_mu(internal_rxn_idxs,solution[internal_rxn_idxs], S)
+    catch 
+    else 
+        @test time >= time_limit
+    end
 end
 
+no_good_cuts_data("iAF692", time_limit=3600)
