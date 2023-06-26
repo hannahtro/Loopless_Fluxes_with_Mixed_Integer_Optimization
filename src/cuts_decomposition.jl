57453921a3af02190bc42854a10b3f0229d94c18
Troppens <htroppen@mob-009564.zib.de>
2023-06-26 15:19:17 +0200

    work on combiatorial benders, tests pass if all reactions are blocked

--- src/cuts_decomposition.jl
+++ src/cuts_decomposition.jl
@@ -20,21 +20,18 @@ function no_good_cuts(model, internal_rxn_idxs, S; time_limit=1800)
         @constraint(model, !a[cidx] => {x[ridx] + eps() <= 0})
     end
     # @objective(model, Max, 0)
-    # println("here")
 
     start_time = time()
-    # println("here")
     dual_bounds = []
 
     objective_value, dual_bound, solution, _, termination = optimize_model(model)
-    # @show solution
+
     push!(dual_bounds, dual_bound)
     solutions = [round.(solution, digits=5)]
     cuts = []
-    # print(model)
+
     iter = 1
     while !thermo_feasible_mu(internal_rxn_idxs,solution[internal_rxn_idxs], S) && time()-start_time < time_limit
-        # @show thermo_feasible_mu(internal_rxn_idxs,solution[internal_rxn_idxs], S)
         solution_a = solution[num_reactions+1:end]
         @assert round.(solution_a) == solution_a
 
@@ -47,12 +44,10 @@ function no_good_cuts(model, internal_rxn_idxs, S; time_limit=1800)
                 push!(Z,idx)
             end
         end 
-        # @show Z,O
 
         cut = @constraint(model, sum(a[O]) + sum([1-a[i] for i in Z]) <= length(internal_rxn_idxs)-1)
         @assert !(cut in cuts)
         push!(cuts,[cut])
-        # print(model)
 
         objective_value, dual_bound, solution, _, termination = optimize_model(model)
         solution = round.(solution, digits=5)
@@ -65,8 +60,7 @@ function no_good_cuts(model, internal_rxn_idxs, S; time_limit=1800)
         push!(dual_bounds, dual_bound)
         iter += 1
     end
-    # @show cuts
-    # @show solutions
+
     end_time = time()
     time_taken = end_time - start_time
     # @show time_taken
@@ -107,6 +101,9 @@ function no_good_cuts_data(organism; time_limit=1800, csv=true)
     end
 end
 
+"""
+build master problem of combinatorial Benders decomposition with FBA constraints and indicator variables
+"""
 function build_master_problem(master_problem, internal_rxn_idxs)
     set_attribute(master_problem, MOI.Silent(), true)
     x = master_problem[:x]
@@ -118,108 +115,82 @@ function build_master_problem(master_problem, internal_rxn_idxs)
         @constraint(master_problem, a[cidx] => {x[ridx] - eps() >= 0})
         @constraint(master_problem, !a[cidx] => {x[ridx] + eps() <= 0})
     end
-    # print(master_problem)
 end
 
-# TODO: pass MIS
+"""
+build sub problem of combinatorial Benders decomposition including the thermodynamic constraints on the indicator variables 
+for a given solution to the master problem and the minimal infeasible subset C
+"""
 function build_sub_problem(sub_problem, internal_rxn_idxs, S, solution_a, C)
+    @show solution_a
+    @show C
     set_attribute(sub_problem, MOI.Silent(), true)
     set_objective_sense(sub_problem, MAX_SENSE)
     S_int = Array(S[:, internal_rxn_idxs])
 
     G = @variable(sub_problem, G[1:length(internal_rxn_idxs)])
-    μ = @variable(sub_problem, μ[1:size(S)[1]])
+    μ = @variable(sub_problem, μ[1:size(S_int)[1]])
     constraint_list = []
-    for (idx,val) in enumerate(solution_a)
-        if val == 0
+    for (idx,val) in enumerate(solution_a) 
+        if val == 0 && (idx in C)
             c = @constraint(sub_problem, G[idx] <= -1) #TODO: check loopless fba formulation
             push!(constraint_list,c)
-        elseif val == 1
+        elseif val == 1 && (idx in C)
             c = @constraint(sub_problem, G[idx] >= 1)      
             push!(constraint_list,c)
+        else
+            @assert (idx in C) == false
         end
     end
-    c_matrix = @constraint(sub_problem, G' .== μ' * S_int)
-    # push!(constraint_list,c)
-    # print(sub_problem)
+    c_matrix = @constraint(sub_problem, G .== S_int' * μ)
+
     return constraint_list, c_matrix
 end
 
-# TODO: implement fast MIS Search
-# TODO: block zeros and ones??
+"""
+returns a minimal infeasible subset of reactions for a give soltion and stoichiometric matrix
+"""
 # TODO: compute several MISs at once
 function compute_MIS(solution_a, S_int; fast=true)
-    # build mis_search_primal
-    # build mis_search_dual
-    # add objective with weights
-    # C = 1:length(solution_a)
     if !fast
-        C = [idx for (idx,val) in enumerate(solution_a) if val==1]
+        C = [idx for (idx,val) in enumerate(solution_a)]
     else 
-        @show solution_a
-        @show S_int
-        @show S_int'
-
+        # build mis_search_dual
         A = deepcopy(S_int')
         for (idx,a) in enumerate(solution_a)
             if a == 0
                 A[idx,:] = - A[idx,:] # update rows
             end
         end
-        @show S_int'
-        @show A
-        # @show A[3,:] #TODO verify idex
 
-        b = [1 for i in 1:size(S_int)[1]]
+        b = [0.001 for i in 1:size(S_int)[1]]
 
-        @show b
-        C = []
+        mis_model = Model(HiGHS.Optimizer)
+        set_attribute(mis_model, MOI.Silent(), true)
 
-        mis_model = Model(SCIP.Optimizer)
         @variable(mis_model, λ[1:length(b)])
         @constraint(mis_model, λ .>= 0)
-        @constraint(mis_model, A' * λ .== 0)
+        @constraint(mis_model, A * λ .== 0)
         @constraint(mis_model, b'*λ==1)
+
+        # add objective with weights
         @objective(mis_model, Min, sum(λ))
 
-        print(mis_model)
         optimize!(mis_model)
+        @assert termination_status(mis_model) == MOI.OPTIMAL
+        # @show MOI.get(mis_model, MOI.ObjectiveValue())
+        solution = [value(var) for var in all_variables(mis_model)]
 
-        @show termination_status(mis_model)
-        @show MOI.get(mis_model, MOI.ObjectiveValue())
-        @show [value(var) for var in all_variables(mis_model)]
-
-
-        # @show MOI.get(sub_problem, MOI.ObjectiveBound())
-        # # @show result_count(sub_problem)
-        # @show dual_objective_value(sub_problem)
-
-        # dual_values = []
-        # for c in constraint_list
-        #     push!(dual_values, dual(c))
-        # end
-        # append!(dual_values, dual.(c_matrix))
-        # @show dual_values
-
-        # dual_sub_problem = dualize(sub_problem, HiGHS.Optimizer; dual_names = DualNames("dual", ""))
-        # print(dual_sub_problem)
-        # optimize!(dual_sub_problem)
-        # @show MOI.get(dual_sub_problem, MOI.ObjectiveValue())
-
-        # obj = objective_function(dual_sub_problem)
-        # @constraint(dual_sub_problem, obj==1)
-        # @objective(dual_sub_problem, Min, 0)
-
-        # print(dual_sub_problem)
-        # optimize!(dual_sub_problem)
-        # @show MOI.get(dual_sub_problem, MOI.ObjectiveValue())
-        # repeat until solution is feasible
-        # @show C
+        C = [idx for (idx,val) in enumerate(solution) if !(isapprox(val,0))]
     end
     return C
 end
 
-function add_combinatorial_benders_cut(master_problem, solution_a, C; fast=true)
+"""
+adds combinatorial Benders' cut to the master problem, by forcing a different assignment of the indicator variables
+of the reactions in the minimal infeasible subset C
+"""
+function add_combinatorial_benders_cut(master_problem, solution_a, C)
     a = master_problem[:a]
     Z = []
     O = []
@@ -240,16 +211,24 @@ function add_combinatorial_benders_cut(master_problem, solution_a, C; fast=true)
     end
 end 
 
-function combinatorial_benders(master_problem, internal_rxn_idxs, S; max_iter=Inf, fast=true)
+"""
+solve problem by splitting it into a master problem with indicator variables and a linear sub problem based 
+on a solution to the master problem and minimal infeasible subsets. The sub problem 
+"""
+function combinatorial_benders(master_problem, internal_rxn_idxs, S; max_iter=Inf, fast=true, time_limit=1800)
     @show fast
-    m, num_reactions = size(S)
+    _, num_reactions = size(S)
+
+    start_time = time()
+    dual_bounds = []
 
     # solve master problem
     build_master_problem(master_problem, internal_rxn_idxs)   
     objective_value_master, dual_bound_master, solution_master, _, termination_master = optimize_model(master_problem)
     solutions = [solution_master]
     solution_a = solution_master[num_reactions+1:end]
-    
+    push!(dual_bounds, dual_bound_master)
+
     # compute corresponding MIS
     S_int = Array(S[:, internal_rxn_idxs])
     C = compute_MIS(solution_a, fast=fast, S_int)
@@ -261,24 +240,71 @@ function combinatorial_benders(master_problem, internal_rxn_idxs, S; max_iter=In
 
     objective_value_sub, dual_bound_sub, solution_sub, _, termination_sub = optimize_model(sub_problem, silent=false)
 
-    # if infeasible, add Benders' cut
+    # add Benders' cut if subproblem is infeasible
     iter = 1
-    while termination_sub == MOI.INFEASIBLE && iter < max_iter
+    while termination_sub == MOI.INFEASIBLE && iter <= max_iter && time()-start_time < time_limit
+        @show iter
         @assert primal_status(sub_problem) == MOI.NO_SOLUTION
         @assert dual_status(sub_problem) == MOI.INFEASIBILITY_CERTIFICATE
         @assert has_duals(sub_problem)
 
-        add_combinatorial_benders_cut(master_problem, solution_a, C) 
+        # add CB cut to MP and solve MP
+        add_combinatorial_benders_cut(master_problem, solution_a, C)
         objective_value_master, dual_bound_master, solution_master, _, termination_master = optimize_model(master_problem)
         @assert !(solution_master in solutions)
         push!(solutions, solution_master)
+        push!(dual_bounds, dual_bound_master)
         solution_a = solution_master[num_reactions+1:end]
         # @show solution_a
 
+        # compute corresponding MIS
+        C = compute_MIS(solution_a, fast=fast, S_int)
+
+        # build sub problem to master solution 
         sub_problem = Model(optimizer)
         constraint_list, c_matrix = build_sub_problem(sub_problem, internal_rxn_idxs, S, solution_a, C)
         objective_value_sub, dual_bound_sub, solution_sub, _, termination_sub = optimize_model(sub_problem, silent=false)
 
         iter += 1
+    end
+    end_time = time()
+    time_taken = end_time - start_time
+    solution = vcat(solution_master, solution_sub)
+    @show termination_sub
+
+    return objective_value_master, dual_bounds, solution, time_taken, termination_sub, iter
+end
+
+function combinatorial_benders_data(organism; time_limit=1800, csv=true, max_iter=Inf, fast=true)
+    @show fast
+    model = deserialize("../data/" * organism * ".js")
+    print_model(model, "organism")
+
+    S = stoichiometry(model)
+    lb, ub = bounds(model)
+    internal_rxn_idxs = [
+        ridx for (ridx, rid) in enumerate(variables(model)) if
+        !is_boundary(reaction_stoichiometry(model, rid))
+    ]
+
+    master_problem = build_fba_model(S, lb, ub)
+    objective_value, dual_bounds, solution, time, termination, iter = combinatorial_benders(master_problem, internal_rxn_idxs, S, max_iter=max_iter, fast=fast)
+
+    @show max_iter, time_limit
+    @show objective_value
+    df = DataFrame(
+        objective_value=objective_value, 
+        dual_bounds=[dual_bounds],
+        solution=[solution], 
+        time=time, 
+        termination=termination,
+        time_limit=time_limit, 
+        thermo_feasible=thermo_feasible,
+        iter=iter)
+
+    type = "combinatorial_benders"
+    file_name = joinpath(@__DIR__,"../csv/" * organism * "_" * type * "_" * string(time_limit) * ".csv")
+    if csv
+        CSV.write(file_name, df, append=false, writeheader=true)
     end
 end
--- test/no_good_cuts.jl
+++ test/no_good_cuts.jl
@@ -5,58 +5,75 @@ using CSV
 include("../src/cuts_decomposition.jl")
 
 @testset "simple model" begin
-    S = [[1,0,0] [-1,1,0] [0,-1,1] [-1,0,1] [0,0,-1]]
-    lb = [0,-10,-10,-10,0]
-    ub = [10,30,30,30,10]
+    S = [[1,0,0,0] [-1,1,0,0] [0,-1,1,0] [-1,0,1,0] [0,0,-1,0] [-1,0,0,1] [0,0,1,-1]]
+    lb = [0,-10,-10,-10,0,0,0]
+    ub = [10,30,30,30,10,10,10]
     m, num_reactions = size(S)
     @show m, num_reactions
 
     model = build_fba_model(S, lb, ub)
-    internal_rxn_idxs = [2,3,4]
+    internal_rxn_idxs = [2,3,4,6,7]
 
     # no good cuts
     objective_value, dual_bound, solution, time, termination, iter = no_good_cuts(model, internal_rxn_idxs, S)
     @test thermo_feasible_mu(internal_rxn_idxs,solution[internal_rxn_idxs], S)
 
     # combinatorial Benders'
+    # TODO: solution not thermo feasible
     model = build_fba_model(S, lb, ub)
-    combinatorial_benders(model, internal_rxn_idxs, S, fast=false)
+    objective_value, dual_bounds, solution, time, termination, iter = combinatorial_benders(model, internal_rxn_idxs, S, fast=false)
+    @show solution
+    @show objective_value
+    feasible = thermo_feasible(internal_rxn_idxs, solution[internal_rxn_idxs], S)
+    @test feasible
+    println("--------------------------------------------------------")
 
-    # fast combinatorial Benders'
-    model = build_fba_model(S, lb, ub)
-    @show S
-    combinatorial_benders(model, internal_rxn_idxs, S, fast=true)
+    # # fast combinatorial Benders'
+    # model = build_fba_model(S, lb, ub)
+    # @show S
+    # objective_value, dual_bounds, solution, time, termination, iter = combinatorial_benders(model, internal_rxn_idxs, S, fast=true)
+    # @show solution
+    # @show objective_value
+    # feasible = thermo_feasible(internal_rxn_idxs, solution[internal_rxn_idxs], S)
+    # @test feasible
 end
 
-# TODO: does not terminate in 200 iterations: verify that solution is eventually found
-@testset "iAF692" begin
-    organism = "iAF692"
-    model = deserialize("../data/" * organism * ".js")
-    print_model(model, "organism")
+# # TODO: no good cuts approach does not terminate in 200 iterations: verify that solution is eventually found
+# @testset "iAF692" begin
+#     organism = "iAF692"
+#     model = deserialize("../data/" * organism * ".js")
+#     print_model(model, "organism")
 
-    S = stoichiometry(model)
-    lb, ub = bounds(model)
-    internal_rxn_idxs = [
-        ridx for (ridx, rid) in enumerate(variables(model)) if
-        !is_boundary(reaction_stoichiometry(model, rid))
-    ]
+#     S = stoichiometry(model)
+#     lb, ub = bounds(model)
+#     internal_rxn_idxs = [
+#         ridx for (ridx, rid) in enumerate(variables(model)) if
+#         !is_boundary(reaction_stoichiometry(model, rid))
+#     ]
 
-    model = build_fba_model(S, lb, ub)
+    # model = build_fba_model(S, lb, ub)
 
-    time_limit = 2
-    objective_value, dual_bound, solution, time, termination, iter = no_good_cuts(model, internal_rxn_idxs, S, time_limit=time_limit)
+    # time_limit = 2
+    # objective_value, dual_bound, solution, time, termination, iter = no_good_cuts(model, internal_rxn_idxs, S, time_limit=time_limit)
 
-    try 
-        thermo_feasible_mu(internal_rxn_idxs,solution[internal_rxn_idxs], S)
-    catch 
-    else 
-        @test time >= time_limit
-    end
+    # try 
+    #     thermo_feasible_mu(internal_rxn_idxs,solution[internal_rxn_idxs], S)
+    # catch 
+    # else 
+    #     @test time >= time_limit
+    # end
 
-    # combinatorial Benders'
-    model = build_fba_model(S, lb, ub)
-    combinatorial_benders(model, internal_rxn_idxs, S, max_iter=10, fast=false)
-end
+#     # combinatorial Benders'
+#     model = build_fba_model(S, lb, ub)
+#     combinatorial_benders(model, internal_rxn_idxs, S, max_iter=10, fast=false)
+
+#     # fast combinatorial Benders'
+#     model = build_fba_model(S, lb, ub)
+#     combinatorial_benders(model, internal_rxn_idxs, S, max_iter=10, fast=true)
+# end
 
 # no_good_cuts_data("iAF692", time_limit=3600)
 
+# println("--------------------------------------------------------")
+# combinatorial_benders_data("iAF692", max_iter=10, time_limit=30, csv=false, fast=false)
+# combinatorial_benders_data("iAF692", max_iter=10, time_limit=30, csv=false, fast=true)
