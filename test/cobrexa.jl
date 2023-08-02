9ce92ed1d2b860de5022e407da12043960238c11
hannahtro <hannahtroppens@gmail.com>
2023-08-02 16:01:37 +0200

    cobrexa data

--- test/cobrexa.jl
+++ test/cobrexa.jl
@@ -1,33 +1,40 @@
 include("../src/cobrexa.jl")
 
-organism = "iAF692"
-fba_data(organism)
-loopless_fba_data(organism, time_limit=600)
+#organism = "iAF692"
+#@show organism
+#fba_data(organism)
+#loopless_fba_data(organism, time_limit=600)
 
-organism = "iJR904"
-fba_data(organism)
-loopless_fba_data(organism, time_limit=600)
+#organism = "iJR904"
+#@show organism
+#fba_data(organism)
+#loopless_fba_data(organism, time_limit=600)
 
-organism = "iML1515"
-fba_data(organism)
-loopless_fba_data(organism, time_limit=600)
+#organism = "iML1515"
+#fba_data(organism)
+#loopless_fba_data(organism, time_limit=600)
 
 organism = "e_coli_core"
+@show organism
 fba_data(organism)
 loopless_fba_data(organism, time_limit=600)
 
 organism = "iNF517"
+@show organism
 fba_data(organism)
 loopless_fba_data(organism, time_limit=600)
 
-organism = "iSB619"
-fba_data(organism)
-loopless_fba_data(organism, time_limit=600)
+#organism = "iSB619"
+#@show organism
+#fba_data(organism)
+#loopless_fba_data(organism, time_limit=600)
 
 organism = "iNJ661"
+@show organism
 fba_data(organism)
 loopless_fba_data(organism, time_limit=600)
 
 organism = "iCN900"
+@show organism
 fba_data(organism)
 loopless_fba_data(organism, time_limit=600)
