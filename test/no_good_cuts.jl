656a3995976f4fba6a4f7e621dcf5f8b999b937f
Troppens <htroppen@mob-009564.zib.de>
2023-08-08 09:25:07 +0200

    prepare run

--- test/no_good_cuts.jl
+++ test/no_good_cuts.jl
@@ -159,56 +159,56 @@ println("--------------------------------------------------------")
 # combinatorial_benders_data(organism, time_limit=600, csv=false, fast=true, silent=false)
 # no_good_cuts_data(organism, time_limit=60)
 
-organism = "iJR904"
+# organism = "iJR904"
 # combinatorial_benders_data(organism, time_limit=600, csv=true, fast=false, silent=true)
-combinatorial_benders_data(organism, time_limit=600, csv=true, fast=true, silent=true)
+# combinatorial_benders_data(organism, time_limit=600, csv=true, fast=true, silent=true)
 # no_good_cuts_data(organism, time_limit=60)
 
-# organisms = ["iAF692", "iJR904", "iML1515", "e_coli_core", "iNF517", "iSB619", "iNJ661", "iCN900"]
+organisms = ["e_coli_core", "iAF692", "iJR904", "iML1515", "iNF517", "iSB619", "iNJ661", "iCN900"]
 
-# for organism in organisms
-#     type = "no_good_cut"
-#     try 
-#         no_good_cuts_data(organism, time_limit=600)
-#     catch e 
-#         println(e)
-#         file = organism * "_" * type
-#         open(file * ".txt","a") do io
-#             println(io, e)
-#         end
-#     end
+for organism in organisms
+    type = "no_good_cut"
+    try 
+        no_good_cuts_data(organism, time_limit=600)
+    catch e 
+        println(e)
+        file = organism * "_" * type
+        open(file * ".txt","a") do io
+            println(io, e)
+        end
+    end
 
-#     type = "cb"
-#     try 
-#         combinatorial_benders_data(organism, time_limit=600, fast=false)
-#     catch e 
-#         println(e)
-#         file = organism * "_" * type
-#         open(file * ".txt","a") do io
-#             println(io, e)
-#         end
-#     end
+    type = "cb"
+    try 
+        combinatorial_benders_data(organism, time_limit=600, fast=false)
+    catch e 
+        println(e)
+        file = organism * "_" * type
+        open(file * ".txt","a") do io
+            println(io, e)
+        end
+    end
 
-#     type = "cb_fast"
-#     try 
-#         combinatorial_benders_data(organism, time_limit=600, fast=true)
-#     catch e 
-#         println(e)
-#         file = organism * "_" * type
-#         open(file * ".txt","a") do io
-#             println(io, e)
-#         end
-#     end
+    type = "cb_fast"
+    try 
+        combinatorial_benders_data(organism, time_limit=600, fast=true)
+    catch e 
+        println(e)
+        file = organism * "_" * type
+        open(file * ".txt","a") do io
+            println(io, e)
+        end
+    end
     
-# #    type = "ch"
-# #    try 
-# #        constraint_handler_data(organism, time_limit=600)
-# #    catch e 
-# #        println(e)
-# #        file = organism * "_" * type
-# #        open(file * ".txt","a") do io
-# #            println(io, e)
-# #        end
-# #    end
-# end
+#    type = "ch"
+#    try 
+#        constraint_handler_data(organism, time_limit=600)
+#    catch e 
+#        println(e)
+#        file = organism * "_" * type
+#        open(file * ".txt","a") do io
+#            println(io, e)
+#        end
+#    end
+end
 
