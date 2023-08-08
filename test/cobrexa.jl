43015e06a9dad26f51695ee4e7614dbeec04002d
Troppens <htroppen@mob-009564.zib.de>
2023-08-08 16:51:45 +0200

    set time limit in cobrexa

--- test/cobrexa.jl
+++ test/cobrexa.jl
@@ -1,18 +1,18 @@
 include("../src/cobrexa.jl")
 
-#organism = "iAF692"
-#@show organism
-#fba_data(organism)
-#loopless_fba_data(organism, time_limit=600)
+organism = "iAF692"
+@show organism
+fba_data(organism)
+loopless_fba_data(organism, time_limit=600)
 
-#organism = "iJR904"
-#@show organism
-#fba_data(organism)
-#loopless_fba_data(organism, time_limit=600)
+organism = "iJR904"
+@show organism
+fba_data(organism)
+loopless_fba_data(organism, time_limit=600)
 
-#organism = "iML1515"
-#fba_data(organism)
-#loopless_fba_data(organism, time_limit=600)
+organism = "iML1515"
+fba_data(organism)
+loopless_fba_data(organism, time_limit=600)
 
 organism = "e_coli_core"
 @show organism
