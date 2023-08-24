cc3e915eec18c3df8f077c4da2997143b3bd0a5a
Troppens <htroppen@mob-009564.zib.de>
2023-08-24 11:26:32 +0200

    setup z1 test run

--- experiments/run_experiments.jl
+++ experiments/run_experiments.jl
@@ -1,4 +1,30 @@
-include("loopless_fba.jl")
-include("no_good_cuts.jl")
-# include("cobrexa.jl")
-# include("fba.jl")
+organisms = [
+    "iAF692", 
+    # "iJR904", 
+    # "iML1515", 
+    # "e_coli_core",
+    # "iNF517",
+    # "iSB619",
+    # "iNJ661",
+    # "iCN900",
+    # "iAF1260",
+    # "iEK1008",
+    # "iJO1366",
+    # "iMM904",
+    # "iSDY_1059",
+    # "iSFV_1184",
+    # "iSF_1195",
+    # "iS_1188",
+    # "iSbBS512_1146"
+]
+
+time_limit = 1200
+fast = true
+json = true
+yeast = false
+
+for organism in organisms
+    @show organism
+    run(`sbatch -A ais2t batch.sh $organism $time_limit $fast $json $yeast`)
+    # run(`sh batch.sh $organism $time_limit $fast $json $yeast`)
+end
