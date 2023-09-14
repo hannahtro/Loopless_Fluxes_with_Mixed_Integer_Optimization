9373046a70271631c8d71f4bd7eeede2a8992323
Troppens <htroppen@htc-login002.zib.de>
2023-09-14 14:09:49 +0200

    prepare non yeast model run with Gurobi

--- experiments/run_experiments.jl
+++ experiments/run_experiments.jl
@@ -1,31 +1,31 @@
 using DelimitedFiles
 
-# organisms = [
-#     "iAF692", 
-#     # "iJR904", 
-#     # "iML1515", 
-#     # "e_coli_core",
-#     # "iNF517",
-#     # "iSB619",
-#     # "iNJ661",
-#     # "iCN900",
-#     # "iAF1260",
-#     # "iEK1008",
-#     # "iJO1366",
-#     # "iMM904",
-#     # "iSDY_1059",
-#     # "iSFV_1184",
-#     # "iSF_1195",
-#     # "iS_1188",
-#     # "iSbBS512_1146"
-# ]
+organisms = [
+    "iAF692", 
+    "iJR904", 
+    "iML1515", 
+    "e_coli_core",
+    "iNF517",
+    "iSB619",
+    "iNJ661",
+    "iCN900",
+    "iAF1260",
+    "iEK1008",
+    "iJO1366",
+    "iMM904",
+    "iSDY_1059",
+    "iSFV_1184",
+    "iSF_1195",
+    "iS_1188",
+    "iSbBS512_1146"
+]
 
-organisms = readdlm("../molecular_models/ecModel_small_model_names.txt", '\t', String, '\n')
+# organisms = readdlm("../molecular_models/ecModel_small_model_names.txt", '\t', String, '\n')
 
-time_limit = 3600*10
+time_limit = 1800
 fast = true
 json = true
-yeast = true
+yeast = false
 
 for organism in organisms
     @show organism
