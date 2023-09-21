e0061f6fedfef808a97885f00623dbbf882bfd7d
hannahtro <jaimelabaguette@gmail.com>
2023-09-21 12:13:45 +0200

    add #cuts and #iter to csv

--- experiments/store_different_solver_results.jl
+++ experiments/store_different_solver_results.jl
@@ -3,7 +3,7 @@ using DataFrames, JSON, CSV
 """
 compare ll-FBA, no good cuts and fast combinatorial Benders
 """
-function gurobi_data(organisms; no_good_cuts=false, fba=false, cobrexa=false, cb=false, cb_big_m=false, yeast=false, nullspace=false, time_limit=1800, solver="SCIP")
+function solver_data(organisms; no_good_cuts=false, fba=false, cobrexa=false, cb=false, cb_big_m=false, yeast=false, nullspace=false, mis_numbers=[], time_limit=1800, solver="SCIP")
     if solver == "" || solver == "SCIP"
         file = string(time_limit) * ".json"
     else 
@@ -24,6 +24,8 @@ function gurobi_data(organisms; no_good_cuts=false, fba=false, cobrexa=false, cb
         df[!, "objective_value_no_good_cuts"] = Float64[] 
         df[!, "time_no_good_cuts"] = Float64[] 
         df[!, "feasibility_no_good_cuts"] = Bool[]
+        df[!, "cuts_no_good_cuts"] = Int64[]
+        df[!, "iter_no_good_cuts"] = Int64[]
     end 
 
     if cb 
@@ -31,6 +33,8 @@ function gurobi_data(organisms; no_good_cuts=false, fba=false, cobrexa=false, cb
         df[!, "objective_value_cb"] = Float64[] 
         df[!, "time_cb"] = Float64[] 
         df[!, "feasibility_cb"] = Bool[]
+        df[!, "cuts_cb"] = Int64[]
+        df[!, "iter_cb"] = Int64[]
     end
 
     if cb_big_m 
@@ -38,6 +42,8 @@ function gurobi_data(organisms; no_good_cuts=false, fba=false, cobrexa=false, cb
         df[!, "objective_value_cb_big_m"] = Float64[] 
         df[!, "time_cb_big_m"] = Float64[] 
         df[!, "feasibility_cb_big_m"] = Bool[]
+        df[!, "cuts_cb_big_m"] = Int64[]
+        df[!, "cuts_cb_big_m"] = Int64[]
     end
 
     if nullspace 
@@ -59,6 +65,15 @@ function gurobi_data(organisms; no_good_cuts=false, fba=false, cobrexa=false, cb
         df[!, "objective_value_ll_fba_cobrexa"] = Float64[] 
         df[!, "time_ll_fba_cobrexa"] = Float64[] 
         df[!, "feasibility_cobrexa"] = Bool[]
+    end 
+
+    for mis in mis_numbers
+        df[!, "termination_cb_mis_" * string(mis)] = String[] 
+        df[!, "objective_value_cb_mis_" * string(mis)] = Float64[] 
+        df[!, "time_cb_mis_" * string(mis)] = Float64[] 
+        df[!, "feasibility_cb_mis_" * string(mis)] = Bool[]
+        df[!, "cuts_cb_mis_" *string(mis)] = Int64[]
+        df[!, "iter_cb_mis_" *string(mis)] = Int64[]
     end 
     
     # @show df
@@ -106,6 +121,8 @@ function gurobi_data(organisms; no_good_cuts=false, fba=false, cobrexa=false, cb
             dict_organism[:objective_value_no_good_cuts] = dict["objective_value"]
             dict_organism[:time_no_good_cuts] = dict["time"]
             dict_organism[:feasibility_no_good_cuts] = dict["thermo_feasible"]
+            dict_organism[:cuts_no_good_cuts] = dict["cuts"]
+            dict_organism[:iter_no_good_cuts] = dict["iter"]
         end 
 
         if cb 
@@ -125,6 +142,8 @@ function gurobi_data(organisms; no_good_cuts=false, fba=false, cobrexa=false, cb
             dict_organism[:objective_value_cb] = dict["objective_value"]
             dict_organism[:time_cb] = dict["time"]
             dict_organism[:feasibility_cb] = dict["thermo_feasible"]
+            dict_organism[:cuts_cb] = dict["cuts"]
+            dict_organism[:iter_cb] = dict["iter"]
         end 
 
         if cb_big_m
@@ -133,6 +152,8 @@ function gurobi_data(organisms; no_good_cuts=false, fba=false, cobrexa=false, cb
             dict_organism[:objective_value_cb_big_m] = dict["objective_value"]
             dict_organism[:time_cb_big_m] = dict["time"]
             dict_organism[:feasibility_cb_big_m] = dict["thermo_feasible"]
+            dict_organism[:cuts_cb_big_m] = dict["cuts"]
+            dict_organism[:iter_cb_big_m] = dict["iter"]
         end 
 
         if fba 
@@ -141,6 +162,16 @@ function gurobi_data(organisms; no_good_cuts=false, fba=false, cobrexa=false, cb
             dict_organism[:objective_value_fba] = dict["objective_value"]
             dict_organism[:time_fba] = dict["time"]
             dict_organism[:feasibility_fba] = dict["thermo_feasible"]
+        end 
+
+        for mis in mis_numbers
+            dict = JSON.parse(open("json/" * organism * "_combinatorial_benders_fast_" * string(mis) * "_mis_" * file))
+            dict_organism[Symbol("termination_cb_mis_" * string(mis))] = dict["termination"]
+            dict_organism[Symbol("objective_value_cb_mis_" * string(mis))] = dict["objective_value"]
+            dict_organism[Symbol("time_cb_mis_" * string(mis))] = dict["time"]
+            dict_organism[Symbol("feasibility_cb_mis_" * string(mis))] = dict["thermo_feasible"]
+            dict_organism[Symbol("cuts_cb_mis_" * string(mis))] = dict["cuts"]
+            dict_organism[Symbol("iter_cb_mis_" * string(mis))] = dict["iter"]
         end 
 
         for (key, value) in dict_organism
@@ -207,21 +238,21 @@ function gurobi_data(organisms; no_good_cuts=false, fba=false, cobrexa=false, cb
     # @show df[!, [:time_ll_fba, :time_no_good_cuts, :time_cb]]
     
     if yeast
-        file_name = "results_yeast_gurobi.csv"
+        file_name = "results_yeast_" * solver * ".csv"
     else
-        file_name = "results_bigg_gurobi.csv"
+        file_name = "results_bigg_" * solver * ".csv"
     end 
     CSV.write("csv/" * file_name, df, append=false, writeheader=true)
 end
 
 # organisms = ["iAF692", "e_coli_core", "iJR904", "iML1515", "iNF517", "iNJ661", "iCN900"] # "iSB619" not feasible
 organisms = [
-    "iAF692", # recompute for 1e-5
+    #"iAF692", # recompute for 1e-5
     "iJR904", 
     "iML1515", 
     "e_coli_core",
     "iNF517",
-    "iSB619", # AssertionError("feasible")
+    # "iSB619", # AssertionError("feasible")
     "iNJ661",
     "iCN900",
     "iAF1260",
@@ -230,22 +261,23 @@ organisms = [
     "iMM904",
     "iSDY_1059",
     "iSFV_1184", # recompute on cluster
-    "iSF_1195",
+    # "iSF_1195",
     "iS_1188",
     "iSbBS512_1146" # recompute on cluster
 ]
-gurobi_data(organisms, time_limit=1800, yeast=false, cb=false, fba=false, cb_big_m=false, solver="Gurobi")
+mis_numbers = [2, 5, 10, 20]
+solver_data(organisms, time_limit=1800, yeast=false, cb=false, fba=false, cb_big_m=false, mis_numbers=mis_numbers)
 
-organisms = [
-    "Hanseniaspora_uvarum",
-    "yHMPu5000035696_Hanseniaspora_singularis",
-    "yHMPu5000034963_Hanseniaspora_clermontiae",
-    "yHMPu5000035695_Hanseniaspora_pseudoguilliermondii",
-    "yHMPu5000035684_Kloeckera_hatyaiensis",
-    "Eremothecium_sinecaudum",
-    "yHMPu5000035659_Saturnispora_dispora",
-    "Tortispora_caseinolytica",
-    "Starmerella_bombicola_JCM9596",
-    "Eremothecium_gossypii",
-    "Ashbya_aceri"]
-gurobi_data(organisms, time_limit=36000, fba=true, yeast=true, cb=true, cb_big_m=true, solver="Gurobi")
+# organisms = [
+#     "Hanseniaspora_uvarum",
+#     "yHMPu5000035696_Hanseniaspora_singularis",
+#     "yHMPu5000034963_Hanseniaspora_clermontiae",
+#     "yHMPu5000035695_Hanseniaspora_pseudoguilliermondii",
+#     "yHMPu5000035684_Kloeckera_hatyaiensis",
+#     "Eremothecium_sinecaudum",
+#     "yHMPu5000035659_Saturnispora_dispora",
+#     "Tortispora_caseinolytica",
+#     "Starmerella_bombicola_JCM9596",
+#     "Eremothecium_gossypii",
+#     "Ashbya_aceri"]
+# solver_data(organisms, time_limit=36000, fba=true, yeast=true, cb=true, cb_big_m=true, solver="Gurobi")
