7d1838eb4e41c5cf37703d48aa346cc3d279d813
hannahtro <jaimelabaguette@gmail.com>
2023-09-21 10:01:43 +0200

    update test

--- test/no_good_cuts.jl
+++ test/no_good_cuts.jl
@@ -41,7 +41,7 @@ println("============================================================")
     println("combinatorial Benders with no good cuts")
     # combinatorial Benders'
     model = build_fba_model(S, lb, ub, set_objective=true)
-    objective_value_cb, objective_values_cb, dual_bounds_cb, solution_cb, time_cb, termination_cb, iter_cb = combinatorial_benders(model, internal_rxn_idxs, S, lb, ub, fast=false)
+    objective_value_cb, objective_values_cb, dual_bounds_cb, solution_cb, x_cb, a_cb, G_cb, _, time_cb, termination_cb, iter_cb, cuts_cb, _, _, _ = combinatorial_benders(model, internal_rxn_idxs, S, lb, ub, fast=false)
     @show objective_value_cb, solution_cb
     @test termination_cb == MOI.OPTIMAL 
     feasible = thermo_feasible(internal_rxn_idxs, solution_cb[internal_rxn_idxs], S)
@@ -56,7 +56,7 @@ println("============================================================")
     # fast combinatorial Benders'
     model = build_fba_model(S, lb, ub, set_objective=true)
     # print(model)
-    objective_value_fast, objective_values_fast, dual_bounds_fast, solution_fast, time_fast, termination_fast, iter_fast = combinatorial_benders(model, internal_rxn_idxs, S, lb, ub, fast=true)
+    objective_value_fast, objective_values_fast, dual_bounds_fast, solution_fast, x_fast, a_fast, G_fast, _, time_fast, termination_fast, iter_fast, cuts_fast, _, _, _ = combinatorial_benders(model, internal_rxn_idxs, S, lb, ub, fast=true)
     @test termination_fast == MOI.OPTIMAL 
     feasible = thermo_feasible(internal_rxn_idxs, solution_fast[internal_rxn_idxs], S)
     @test feasible
@@ -73,7 +73,7 @@ println("============================================================")
     # fast combinatorial Benders'
     model = build_fba_model(S, lb, ub, set_objective=true)
     # print(model)
-    objective_value_multiple_mis, objective_values_multiple_mis, dual_bounds_multiple_mis, solution_multiple_mis, time_multiple_mis, termination_multiple_mis, iter_multiple_mis = combinatorial_benders(model, internal_rxn_idxs, S, lb, ub, fast=true, multiple_mis=3)
+    objective_value_multiple_mis, objective_values_multiple_mis, dual_bounds_multiple_mis, solution_multiple_mis, x_multiple_mis, a_multiple_mis, G_multiple_mis, _, time_multiple_mis, termination_multiple_mis, iter_multiple_mis, cuts_multiple_mis, _, _, _ = combinatorial_benders(model, internal_rxn_idxs, S, lb, ub, fast=true, multiple_mis=3)
     @test termination_multiple_mis == MOI.OPTIMAL 
     feasible = thermo_feasible(internal_rxn_idxs, solution_multiple_mis[internal_rxn_idxs], S)
     @test feasible
@@ -103,7 +103,7 @@ println("============================================================")
     # fast combinatorial Benders'
     model = build_fba_model(S, lb, ub, set_objective=true)
     # run 2 iterations where solution is not loopless
-    objective_value_fast, objective_values_fast, dual_bounds_fast, solution_fast, time_fast, termination_fast, iter_fast = combinatorial_benders(model, internal_rxn_idxs, S, lb, ub, fast=true, max_iter=2)
+    objective_value_fast, objective_values_fast, dual_bounds_fast, solution_fast, x_fast, a_fast, G_fast, _, time_fast, termination_fast, iter_fast, cuts_fast, _, _, _  = combinatorial_benders(model, internal_rxn_idxs, S, lb, ub, fast=true, max_iter=2)
     flux = solution_fast[1:num_reactions]
     flux_directions = solution_fast[num_reactions+1:num_reactions+length(internal_rxn_idxs)]
     @show flux, flux_directions
@@ -112,7 +112,7 @@ println("============================================================")
     
     # solve till optimal solution found
     model = build_fba_model(S, lb, ub, set_objective=true)
-    objective_value_fast, objective_values_fast, dual_bounds_fast, solution_fast, time_fast, termination_fast, iter_fast = combinatorial_benders(model, internal_rxn_idxs, S, lb, ub, fast=true)
+    objective_value_fast, objective_values_fast, dual_bounds_fast, solution_fast, x_fast, a_fast, G_fast, _, time_fast, termination_fast, iter_fast, cuts_fast, _, _, _  = combinatorial_benders(model, internal_rxn_idxs, S, lb, ub, fast=true)
     flux = solution_fast[1:num_reactions]
     flux_directions = solution_fast[num_reactions+1:num_reactions+length(internal_rxn_idxs)]
     @show flux, flux_directions
