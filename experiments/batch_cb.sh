62eda51a6ae63c17e5faeba8489234c825d466e4
Troppens <htroppen@htc-login002.zib.de>
2023-09-28 16:04:13 +0200

    rerun severl mis at once with additional asserts

--- experiments/batch_cb.sh
+++ experiments/batch_cb.sh
@@ -35,7 +35,7 @@
 # Turn on mail notification. There are many possible self-explaining values:
 # NONE, BEGIN, END, FAIL, ALL (including all aforementioned)
 # For more values, check "man sbatch"
-#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
 
 # You may not place any commands before the last SBATCH directive
 julia --project combinatorial_benders.jl $1 $2 $3 $4 $5 $6 &> cb_$1_$SLURM_JOB_ID.txt
