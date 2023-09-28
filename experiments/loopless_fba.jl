f5d13a6f1a39c15ce5af11c702efa10345e0cdd8
Troppens <htroppen@mob-009564.zib.de>
2023-09-28 16:11:49 +0200

    update ll fba batch pipeline

--- experiments/loopless_fba.jl
+++ experiments/loopless_fba.jl
@@ -1,46 +1,29 @@
-using DelimitedFiles
+using Gurobi, HiGHS
+using Dates 
 
-organisms = [
-    "iAF692", 
-    "iJR904", 
-    "iML1515", 
-    "e_coli_core",
-    "iNF517",
-    "iSB619",
-    "iNJ661",
-    "iCN900",
-    "iAF1260",
-    "iEK1008",
-    "iJO1366",
-    "iMM904",
-    "iSDY_1059",
-    "iSFV_1184",
-    "iSF_1195",
-    "iS_1188",
-    "iSbBS512_1146"
-]
+include("../src/loopless_fba.jl")
 
-#organisms = readdlm("../molecular_models/ecModel_small_model_names.txt", '\t', String, '\n')
-# organisms = [
-#     "Hanseniaspora_uvarum",
-# #    "yHMPu5000035696_Hanseniaspora_singularis",
-#     "yHMPu5000034963_Hanseniaspora_clermontiae",
-#     "yHMPu5000035695_Hanseniaspora_pseudoguilliermondii",
-# #    "yHMPu5000035684_Kloeckera_hatyaiensis",
-#     "Eremothecium_sinecaudum",
-# #    "yHMPu5000035659_Saturnispora_dispora",
-#     "Tortispora_caseinolytica",
-# #    "Starmerella_bombicola_JCM9596",
-# #    "Eremothecium_gossypii",
-# #    "Ashbya_aceri"
-# ]
+organism = ARGS[1]
+@show organism
+time_limit = parse(Int64, ARGS[2])
+json = parse(Bool, ARGS[3])
+yeast = parse(Bool, ARGS[4])
+seed = parse(Int64, ARGS[5])
+@show time_limit, json, yeast
 
-time_limit = 1800
-fast = true
-json = true
-yeast = false
+start_time = time()
 
-for organism in organisms
-    @show organism
-    run(`sbatch -A optimi batch_ll_fba.sh $organism $time_limit $json $yeast`) # ll FBA
+type = "loopless_fba"
+try 
+    loopless_fba_data(organism, time_limit=time_limit, yeast=yeast, nullspace_formulation=false, json=json)
+catch e 
+    println(e)
+    file = organism * "_" * type
+    open(file * ".txt","a") do io
+        println(io, e)
+    end
 end
+
+end_time = time()
+time_taken = end_time - start_time 
+@show time_taken
--- experiments/run_loopless_fba.jl
+++ experiments/run_loopless_fba.jl
@@ -1,1 +1,47 @@
 
+using DelimitedFiles
+
+organisms = [
+    "iAF692", 
+    "iJR904", 
+    "iML1515", 
+    "e_coli_core",
+    "iNF517",
+    "iSB619",
+    "iNJ661",
+    "iCN900",
+    "iAF1260",
+    "iEK1008",
+    "iJO1366",
+    "iMM904",
+    "iSDY_1059",
+    "iSFV_1184",
+    "iSF_1195",
+    "iS_1188",
+    "iSbBS512_1146"
+]
+
+#organisms = readdlm("../molecular_models/ecModel_small_model_names.txt", '\t', String, '\n')
+# organisms = [
+#     "Hanseniaspora_uvarum",
+# #    "yHMPu5000035696_Hanseniaspora_singularis",
+#     "yHMPu5000034963_Hanseniaspora_clermontiae",
+#     "yHMPu5000035695_Hanseniaspora_pseudoguilliermondii",
+# #    "yHMPu5000035684_Kloeckera_hatyaiensis",
+#     "Eremothecium_sinecaudum",
+# #    "yHMPu5000035659_Saturnispora_dispora",
+#     "Tortispora_caseinolytica",
+# #    "Starmerella_bombicola_JCM9596",
+# #    "Eremothecium_gossypii",
+# #    "Ashbya_aceri"
+# ]
+
+time_limit = 1800
+fast = true
+json = true
+yeast = false
+
+for organism in organisms
+    @show organism
+    run(`sh batch.sh $organism $time_limit $fast $json $yeast`)
+end
