0c9e4322b5089b0e2ee0519477f1d3345cc8692c
Troppens <htroppen@htc-login001.zib.de>
2024-05-27 16:56:12 +0200

    4h run ll FBA and CB indicator

--- experiments/batch_cb.sh
+++ experiments/batch_cb.sh
@@ -30,7 +30,7 @@
 # --mem-per-cpu will define memory per CPU/core. Choose one of those.
 ##SBATCH --mem-per-cpu=1500MB
 #SBATCH --mem=30GB    # this one is not in effect, due to the double hash
-#SBATCH --exclusive
+##SBATCH --exclusive
 
 # Turn on mail notification. There are many possible self-explaining values:
 # NONE, BEGIN, END, FAIL, ALL (including all aforementioned)
